ad87865929ef1b8a79bc98f3a09012c07ab63084
zwd <zwd321081@gmail.com>
2018-10-08 17:49:31 +0800

    feat: finish basic flow

--- src/tpl/token.js
+++ src/tpl/token.js
@@ -1,22 +1,82 @@
-
-
 const assert = require('assert');
 
-const input  = '(add 2 (subtract 4 2))';
+const input = '(add 2 (subtract 4 2))';
 
 const tokens = [
-  { type: 'paren',  value: '('        },
-  { type: 'name',   value: 'add'      },
-  { type: 'number', value: '2'        },
-  { type: 'paren',  value: '('        },
-  { type: 'name',   value: 'subtract' },
-  { type: 'number', value: '4'        },
-  { type: 'number', value: '2'        },
-  { type: 'paren',  value: ')'        },
-  { type: 'paren',  value: ')'        }
+    { type: 'paren', value: '(' },
+    { type: 'name', value: 'add' },
+    { type: 'number', value: '2' },
+    { type: 'paren', value: '(' },
+    { type: 'name', value: 'subtract' },
+    { type: 'number', value: '4' },
+    { type: 'number', value: '2' },
+    { type: 'paren', value: ')' },
+    { type: 'paren', value: ')' },
 ];
 
-function tokenizer(input){
+function tokenizer(input) {
+    let index = 0;
+    let tokens = [];
+    while (index < input.length) {
+        let c = input[index];
 
+        if (c == '(') {
+            tokens.push({
+                type: 'paren',
+                value: c,
+            });
+            index++;
+            continue;
+        }
+
+        if (c == ')') {
+            tokens.push({
+                type: 'paren',
+                value: c,
+            });
+            index++;
+            continue;
+        }
+
+        let word = /\w/;
+        if (word.test(c)) {
+            let sequence = '';
+            while (word.test(c)) {
+                sequence += c;
+                c = input[++index];
+            }
+
+            tokens.push({
+                type: 'name',
+                value: sequence,
+            });
+            continue;
+        }
+
+        let number = /\d/;
+        if (number.test(c)) {
+            tokens.push({
+                type: 'number',
+                value: c,
+            });
+            index++;
+            continue;
+        }
+
+        let space = /\\s/;
+        if (space.test(c)) {
+            //donothing
+            index++;
+            continue;
+        }
+
+        throw new Error(` no charactor ${c} `);
+    }
+    return tokens;
 }
-assert.deepStrictEqual(tokenizer(input), tokens, 'Tokenizer should turn `input` string into `tokens` array');
+
+assert.deepStrictEqual(
+    tokenizer(input),
+    tokens,
+    'Tokenizer should turn `input` string into `tokens` array'
+);
