b4aa174cc1ea5985151b7773946cd01bdeaa02dd
zwd <zwd321081@gmail.com>
2018-11-01 15:34:51 +0800

    basic

--- src/tpl/generator.js
+++ src/tpl/generator.js
@@ -1,42 +1,72 @@
-
-const assert = require("assert")
-function generator(ast){
-
+const assert = require('assert');
+function generator(node) {
+    switch (node.type) {
+        case 'CallExpression':
+            return (
+                generator(node.callee) +
+                '(' +
+                generator(node.arguments)
+                    .map(generator)
+                    .join('.') +
+                ')'
+            );
+            return;
+        case 'Program':
+            return node.body.map(generator).join('\n');
+            break;
+        case 'NumberLiteral':
+            return node.value;
+            break;
+        case 'Identifier':
+            return node.name;
+            break;
+        case 'StringLiteral':
+        return '"'+node.value+'"';
+        break;
+    }
 }
 
 const newAst = {
-  type: 'Program',
-  body: [{
-    type: 'ExpressionStatement',
-    expression: {
-      type: 'CallExpression',
-      callee: {
-        type: 'Identifier',
-        name: 'add'
-      },
-      arguments: [{
-        type: 'NumberLiteral',
-        value: '2'
-      }, {
-        type: 'CallExpression',
-        callee: {
-          type: 'Identifier',
-          name: 'subtract'
+    type: 'Program',
+    body: [
+        {
+            type: 'ExpressionStatement',
+            expression: {
+                type: 'CallExpression',
+                callee: {
+                    type: 'Identifier',
+                    name: 'add',
+                },
+                arguments: [
+                    {
+                        type: 'NumberLiteral',
+                        value: '2',
+                    },
+                    {
+                        type: 'CallExpression',
+                        callee: {
+                            type: 'Identifier',
+                            name: 'subtract',
+                        },
+                        arguments: [
+                            {
+                                type: 'NumberLiteral',
+                                value: '4',
+                            },
+                            {
+                                type: 'NumberLiteral',
+                                value: '2',
+                            },
+                        ],
+                    },
+                ],
+            },
         },
-        arguments: [{
-          type: 'NumberLiteral',
-          value: '4'
-        }, {
-          type: 'NumberLiteral',
-          value: '2'
-        }]
-      }]
-    }
-  }]
+    ],
 };
 
 const output = 'add(2, subtract(4, 2));';
 
 const re = generator(newAst);
 
-assert.deepEqual(re,output,"code generator is wrong")
+assert.deepEqual(re, output, 'code generator is wrong');
