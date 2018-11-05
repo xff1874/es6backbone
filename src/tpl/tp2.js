d8f6398c897adfd2ce777a90e6fdef88dd128d4d
zwd <zwd321081@gmail.com>
2018-11-05 15:07:08 +0800

    feat: 模板基本出来了，但是逻辑处理

--- src/tpl/tp2.js
+++ src/tpl/tp2.js
@@ -1,4 +1,4 @@
-const str="<p>Hello, my name is <%this.name%>. I\'m <%this.age%> years old.</p>. My compnay is <%this.info.company%>"
+const str="Hello, my name is <%this.name%> and <%this.age%> years old. My compnay is <%this.info.company%>"
 
 const body =  'console.log("my name is "+ this.name+"age is"+ this.age+"work at"+ this.info.company)';
 // template(str,{
@@ -8,7 +8,15 @@ const body =  'console.log("my name is "+ this.name+"age is"+ this.age+"work at"
 //         company:"netease"
 //     }
 // })
-const template = new Function(body)
+const re = /<%(.+?)%>/g;
+let array,newStr='',cursor=0;
+while(( array=re.exec(str))!=null){
+    newStr += str.slice(cursor,array.index);
+    newStr+= array[1];
+    cursor = array.index+array[0].length;
+}
+
+const template = new Function(''+newStr)
 template.apply({
     name: "zwd",
     age: 29,
