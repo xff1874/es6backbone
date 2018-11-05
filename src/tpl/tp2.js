6d51f3df800b4e6839ba9e9348ae2b725a9abe73
zwd <zwd321081@gmail.com>
2018-11-05 15:16:45 +0800

    feat: 添加 double quoate

--- src/tpl/tp2.js
+++ src/tpl/tp2.js
@@ -9,18 +9,20 @@ const body =  'console.log("my name is "+ this.name+"age is"+ this.age+"work at"
 //     }
 // })
 const re = /<%(.+?)%>/g;
-let array,newStr='',cursor=0;
+let array,newStr='return ',cursor=0;
 while(( array=re.exec(str))!=null){
-    newStr += str.slice(cursor,array.index);
-    newStr+= array[1];
+    newStr += "'"+str.slice(cursor,array.index)+"'";
+    newStr+= '+'+array[1]+'+';
     cursor = array.index+array[0].length;
 }
-
-const template = new Function(''+newStr)
-template.apply({
+newStr= newStr.slice(0,newStr.length-1);
+const template = new Function(newStr)
+const finalRe= template.apply({
     name: "zwd",
     age: 29,
     info:{
         company:"netease"
     }
 })
+
+console.log(finalRe)
